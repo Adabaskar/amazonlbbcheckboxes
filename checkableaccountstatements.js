a98a3dd7c13c7ce660dae7570bfddf1a87b5ea46
Bogumil Bartczak <bogumil.bartczak@gmx.de>
2020-03-01 17:28:54 +0100

    overlooked prime bonus

--- checkableaccountstatements.js
+++ checkableaccountstatements.js
@@ -2,7 +2,7 @@
 //var tabdata = 'table > tbody > tr.tabdata > td + td + td > nobr';
 //var tabdata2 = 'table > tbody > tr.tabdata2 > td + td + td > nobr';
 //var transactionPane = 'kkb-transaction-title-list-item' 
-var transactionValue = 'div.list-item__amount > div.ng-star-inserted:not(.list-item__amount_bonus)' 
+var transactionValue = 'div.list-item__amount > div.ng-star-inserted:not(.undefined)' 
 
 var checkbox = '<input type="checkbox" value="no" style="margin-left: 5px; visibility : visible; display : inline;">'
 var matches = document.querySelectorAll(transactionValue);
